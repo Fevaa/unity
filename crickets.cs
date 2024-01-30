bfb17775c36beb7167bdbb60c55f080b5d512dbf
Fevaa <146878053+Fevaa@users.noreply.github.com>
2024-01-30 20:49:32 +0300

    Add files via upload

---
+++ crickets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    void Update()
+    {
+        // Команда для вывода слова "crickets" в консоль
+        Debug.Log("crickets");
+    }
+}
